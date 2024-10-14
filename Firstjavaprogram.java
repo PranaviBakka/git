a47112e1c5827f9d58dbf057bdf22e7d68411f73
pranavi12 <Nagapranavi12@gmail.com>
2024-10-14 20:07:46 +0530

    commiting txt

---
+++ Firstjavaprogram.java
@@ -0,0 +1,17 @@
+Hello Snehal , 
+Hope you are doing well.
+I am Pranavi. I have 3+ years of experience of DevOps Engineer. I get to know job Opportunity in your Company in which i am interested. I believe I have all required skillsets for the position and can be perfect fit. It will be really great help if you can refer me.
+
+
+
+Hello name ,
+
+I Have gone through the Job Description attached above and it suits me perfectly for my candidature.
+
+Kindly find attached resume for your reference .It would be a pleasure to discuss this Exciting opportunity with you.Looking forward to making this work
+
+
+Thank you & Regards,
+Lekhya Naga Pranavi S.
+Mobile no : 9676861209
+Email id : Lekhyanagapranavi12@gmail.com
